20f4ca2fb76ff6b56b02931f615d6e13849c2240
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2025-09-11 06:52:43 -0300

    dando continuidade no layout desktop

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -8,81 +8,83 @@ const Checkout = () => {
 
   return (
     <div className={styles.container}>
-      <GoBack />
-      <section className={styles.checkout}>
-        <div>
-          <h1>Checkout</h1>
-        </div>
-        <div className={styles.billingDetails}>
+      <div>
+        <GoBack />
+        <section className={styles.checkout}>
           <div>
-            <h2>Billing Details</h2>
+            <h1>Checkout</h1>
           </div>
-          <form>
+          <div className={styles.billingDetails}>
             <div>
-              <label htmlFor="name">Name</label>
-              <input type="text" id="name" name="name" required placeholder='Alexei Ward'/>
+              <h2>Billing Details</h2>
             </div>
-            <div>
-              <label htmlFor="email">Email Address</label>
-              <input type="email" id="email" name="email" required placeholder='alexei@mail.com'/>
-            </div>
-            <div>
-              <label htmlFor="phone">Phone Number</label>
-              <input type="text" id="phone" name="phone" required placeholder='+1 202-555-0136'/>
-            </div>
-          </form>
-        </div>
-        <div className={styles.shoppingInfo}>
-          <div>
-            <h2>Shipping Info</h2>
+            <form>
+              <div>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" required placeholder='Alexei Ward'/>
+              </div>
+              <div>
+                <label htmlFor="email">Email Address</label>
+                <input type="email" id="email" name="email" required placeholder='alexei@mail.com'/>
+              </div>
+              <div>
+                <label htmlFor="phone">Phone Number</label>
+                <input type="text" id="phone" name="phone" required placeholder='+1 202-555-0136'/>
+              </div>
+            </form>
           </div>
-          <form>
+          <div className={styles.shoppingInfo}>
             <div>
-              <label htmlFor="address">Your Address</label>
-              <input type="text" id="address" name="address" required placeholder='1137 Williams Avenue'/>
+              <h2>Shipping Info</h2>
             </div>
-            <div>
-              <label htmlFor="city">ZIP Code</label>
-              <input type="text" id="city" name="city" required placeholder='10001'/>
-            </div>
-            <div>
-              <label htmlFor="zip">City</label>
-              <input type="text" id="zip" name="zip" required placeholder='New York'/>
-            </div>
-            <div>
-              <label htmlFor="country">Country</label>
-              <input type="text" id="country" name="country" required placeholder='United States'/>
-            </div>
-          </form>
-        </div>
-        <div className={styles.payment}>
-          <div>
-            <h2>Payment Details</h2>
+            <form>
+              <div>
+                <label htmlFor="address">Your Address</label>
+                <input type="text" id="address" name="address" required placeholder='1137 Williams Avenue'/>
+              </div>
+              <div>
+                <label htmlFor="city">ZIP Code</label>
+                <input type="text" id="city" name="city" required placeholder='10001'/>
+              </div>
+              <div>
+                <label htmlFor="zip">City</label>
+                <input type="text" id="zip" name="zip" required placeholder='New York'/>
+              </div>
+              <div>
+                <label htmlFor="country">Country</label>
+                <input type="text" id="country" name="country" required placeholder='United States'/>
+              </div>
+            </form>
           </div>
-          <form>
-            <span>Payment Method</span>
-            <div className={styles.paymentMethod}>
-              <input type='radio' id="e-money" name="payment-method" value="e-money" defaultChecked />
-              <label htmlFor="card-name">e-Money</label>
-            </div>
-            <div className={styles.paymentMethod}>
-              <input type='radio' id="cash-on-delivery" name="payment-method" value="cash-on-delivery" />
-              <label htmlFor="cash-on-delivery">Cash on Delivery</label>
-            </div>
-          </form>
-          <form>
+          <div className={styles.payment}>
             <div>
-              <label htmlFor="e-money-number">e-Money Number</label>
-              <input type="text" id="e-money-number" name="e-money-number" placeholder="238521993" />
+              <h2>Payment Details</h2>
             </div>
-            <div>
-              <label htmlFor="e-money-pin">e-Money PIN</label>
-              <input type="text" id="e-money-pin" name="e-money-pin" placeholder="6891" />
-            </div>
-          </form>
-        </div>
-      </section>
-      <CartSummary />
+            <form>
+              <span>Payment Method</span>
+              <div className={styles.paymentMethod}>
+                <input type='radio' id="e-money" name="payment-method" value="e-money" defaultChecked />
+                <label htmlFor="card-name">e-Money</label>
+              </div>
+              <div className={styles.paymentMethod}>
+                <input type='radio' id="cash-on-delivery" name="payment-method" value="cash-on-delivery" />
+                <label htmlFor="cash-on-delivery">Cash on Delivery</label>
+              </div>
+            </form>
+            <form>
+              <div>
+                <label htmlFor="e-money-number">e-Money Number</label>
+                <input type="text" id="e-money-number" name="e-money-number" placeholder="238521993" />
+              </div>
+              <div>
+                <label htmlFor="e-money-pin">e-Money PIN</label>
+                <input type="text" id="e-money-pin" name="e-money-pin" placeholder="6891" />
+              </div>
+            </form>
+          </div>
+        </section>
+        <CartSummary />
+      </div>
     </div>
   )
 }
